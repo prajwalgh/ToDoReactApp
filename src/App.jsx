ab8c93abc810a6f18df9b82369c15da81628f7ea
prajwalgh <76061744+prajwalgh@users.noreply.github.com>
2022-12-20 20:52:04 +0530

    basic todo app is ready with no css and with bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  const list=['hw',"jq","as","asas",":assa"];
-  const listitems = list.map((number)=><li>{number}</li>)
+  
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = () => {
+    if (todo!==""){
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+    
+};
+const deleteTodo=(text)=>{
+  const newTodos= todos.filter((todo)=>{
+    return todo!==text;
+
+  });
+  setTodos(newTodos)
+}
   return (
     <div style={{textAlign: "center"}}>
       <div><p>TO DO APP</p></div>
       <div>
-        <ui>
-      
-          {listitems}
-        
-        </ui> 
+      {todos?.length>0 ?(
+        <ul className='todo-list-ul'>
+        {todos.map((todo,index)=>(
+          <div className='todo-item'>
+            <li key={index}>{todo}</li>
+            <button onClick={()=>{deleteTodo(todo);}}className='delete-button'>Delete</button>
+          </div>
+        ))}
+        </ul> ) : (
+          <div className='empty'><p>No task found</p></div>
+        )}
+      </div>
+      <div className='input-wraper'>
+        <input
+         type="text"
+         name="todo"
+         value={todo}
+         placeholder="create a new todo"
+         onChange={(e)=>{
+          setTodo(e.target.value);
+         }}
+          ></input>
+        <button  onClick={addTodo} className='add-button'>Add</button>
       </div>
     </div>
   );
